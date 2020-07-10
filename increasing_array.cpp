d9661f13322f029c169ab8cd05c16351d0f01db3
lorderikstark0 <sagnik.chatterjee61@gmail.com>
2020-07-10 11:01:18 +0530

    added files

---
+++ increasing_array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n ;
+  cin >> n;
+  vector<int> arr;
+  while(n--){
+    int a;
+    cin >> a;
+    arr.push_back(a);
+  }
+  
+  for(size_t i=0;i<arr.size();i++){
+    if(arr[i])
+  }
+
+
+  return 0;
+}
